5bc8254250fe8093867b3dcb4d50b4142d08db22
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-21 14:55:53 -0300

    Refactor game logic

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@ import {
   type Position, 
   createRandomGameBoard, 
   createBoardData, 
-  isMine, 
-  getNeighbors } from './logic/game';
+  checkPosition, } from './logic/game';
 import { MINE } from './constants';
 interface Board {
   board: number[][];
@@ -21,52 +20,8 @@ onMounted(() => {
   overlapBoard.board = Array(size).fill(1).map(() => Array(size).fill(1)) // 1 means overlaped and 0 means that is visible
 })
 
-function checkPosition(position: Position) {
-  console.log(position)
-  // Check if current position is already visited
-  if(overlapBoard.board[position.x][position.y] === 0) return
-
-  // Verify if current position is a mine
-  if(gameBoardData.board[position.x][position.y] === -1) {
-    alert('You lose')
-    return
-  }
-
-  // Verify if current position is a number
-  if (gameBoardData.board[position.x][position.y] > 0) {
-    overlapBoard.board[position.x][position.y] = 0
-    return
-  }
-
-  // Verify if current position is a 0 and we have to show the 0 and the adjacent cells
-  showCellAndCheckAdjacents(position, gameBoardData, overlapBoard, [])
-}
-
-function showCellAndCheckAdjacents(position: Position, gameBoard: Board, overlapBoard: Board, visitedPositions: Position[]) {
-  // First show current position
-  overlapBoard.board[position.x][position.y] = 0
-
-  // Check base case, this means that we can found a number and must return
-  if (gameBoard.board[position.x][position.y] > 0) {
-    return
-  }
-
-  // Set this position as visited
-  visitedPositions.push(position)
-
-  // Get valid neighbords
-  const neighbors = getNeighbors(position, gameBoard.board).filter(position => !visitedPositions.some(visitedPosition => visitedPosition.x === position.x && visitedPosition.y === position.y))
-  neighbors.forEach( (neighbor: Position) => {
-    // We have two posible cases here, is a number or is a 0
-    // If is a number we have to show it and return
-    if (gameBoard.board[neighbor.x][neighbor.y] > 0) {
-      overlapBoard.board[neighbor.x][neighbor.y] = 0
-      return
-    }
-
-    // If if a 0 then we have to make a recursion call to verify it
-    showCellAndCheckAdjacents(neighbor, gameBoard, overlapBoard, visitedPositions)
-  })
+function handleClick(position: Position) {
+  checkPosition(position, gameBoardData.board, overlapBoard.board)
 }
 
 function newGame() {
@@ -82,7 +37,7 @@ function newGame() {
       <div :class="{
           cell: true,
           overlap: overlapBoard.board[i][j] === 1
-      }" v-for="(cell, j) in row" :key="`cell${j}`" @click="checkPosition({x:i, y:j})">
+      }" v-for="(cell, j) in row" :key="`cell${j}`" @click="handleClick({x:i, y:j})">
         {{ cell === -1 ? MINE : cell !== 0 ? `${cell}` : '' }}
       </div>
     </div>
--- src/logic/game.ts
+++ src/logic/game.ts
@@ -66,3 +66,50 @@ export function createRandomGameBoard(size = 10, bombs = 5): number[][] {
   }
   return [...mineBoard]
 }
+
+function showCellAndCheckAdjacents(position: Position, gameBoard: number[][], overlapBoard: number[][], visitedPositions: Position[]) {
+  // First show current position
+  overlapBoard[position.x][position.y] = 0
+
+  // Check base case, this means that we can found a number and must return
+  if (gameBoard[position.x][position.y] > 0) {
+    return
+  }
+
+  // Set this position as visited
+  visitedPositions.push(position)
+
+  // Get valid neighbords
+  const neighbors = getNeighbors(position, gameBoard).filter(position => !visitedPositions.some(visitedPosition => visitedPosition.x === position.x && visitedPosition.y === position.y))
+  neighbors.forEach( (neighbor: Position) => {
+    // We have two posible cases here, is a number or is a 0
+    // If is a number we have to show it and return
+    if (gameBoard[neighbor.x][neighbor.y] > 0) {
+      overlapBoard[neighbor.x][neighbor.y] = 0
+      return
+    }
+
+    // If if a 0 then we have to make a recursion call to verify it
+    showCellAndCheckAdjacents(neighbor, gameBoard, overlapBoard, visitedPositions)
+  })
+}
+
+export function checkPosition(position: Position, gameBoard: number[][], overlapBoard: number[][]) {
+  // Check if current position is already visited
+  if(overlapBoard[position.x][position.y] === 0) return
+
+  // Verify if current position is a mine
+  if(gameBoard[position.x][position.y] === -1) {
+    alert('You lose')
+    return
+  }
+
+  // Verify if current position is a number
+  if (gameBoard[position.x][position.y] > 0) {
+    overlapBoard[position.x][position.y] = 0
+    return
+  }
+
+  // Verify if current position is a 0 and we have to show the 0 and the adjacent cells
+  showCellAndCheckAdjacents(position, gameBoard, overlapBoard, [])
+}
