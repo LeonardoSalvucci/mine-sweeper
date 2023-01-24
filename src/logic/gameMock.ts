f53c218cb58e9efee54d305f216530c84495ac1a
Leonardo Salvucci <le.salvucci@gmail.com>
2023-01-24 16:22:42 -0300

    Added game mock logic for testing purpose

---
+++ src/logic/gameMock.ts
@@ -0,0 +1,13 @@
+const defaultBoard = [
+  [-1, 1, 0],
+  [1,  1, 0],
+  [0,  0, 0],
+]
+
+
+export function  createMockRandomGameBoard(size?: number, bomds?: number): number[][] {
+  return defaultBoard
+}
+export function createMockBoardData(mineBoard: number[][]) {
+  return defaultBoard
+}
